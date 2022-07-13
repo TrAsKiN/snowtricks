e8e0f2e3a6ed96c7a0ed7b38609de3051a9af0b9
TrAsKiN <marchal.simon@gmail.com>
2022-07-13 16:09:30 +0200

    feat: add home controller

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\TrickRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'homepage')]
+    public function home(
+        TrickRepository $trickRepository
+    ): Response {
+        $tricks = $trickRepository->findAll();
+        return $this->render('home/home.html.twig', compact('tricks'));
+    }
+}
