c5e8c626d2943fb89bd9627bc7cce82733a2fb23
MARCHAL Simon <TrAsKiN@users.noreply.github.com>
2022-07-14 21:47:16 +0200

    feat: add home controller

--- assets/app.js
+++ assets/app.js
@@ -4,9 +4,22 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-
-// any CSS you import will output into a single css file (app.css in this case)
+import 'bootstrap';
+import 'bootstrap-icons/font/bootstrap-icons.scss';
 import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+let scrollButton = document.querySelector('#scroll-button');
+window.addEventListener('scroll', ev => {
+    if (window.scrollY >= 0 && window.scrollY < window.outerHeight / 2) {
+        scrollButton.querySelector('.bi-arrow-down').classList.remove('d-none');
+        scrollButton.querySelector('.bi-arrow-up').classList.add('d-none');
+        scrollButton.href = '#tricks';
+    } else if (window.scrollY >= window.outerHeight / 2) {
+        scrollButton.querySelector('.bi-arrow-down').classList.add('d-none');
+        scrollButton.querySelector('.bi-arrow-up').classList.remove('d-none');
+        scrollButton.href = '#';
+    }
+});
