cdb4bf4142c88eb302a287a4cf9cb029d39f6be7
TrAsKiN <marchal.simon@gmail.com>
2022-08-04 15:08:30 +0200

    feat: add pagination

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,9 +12,16 @@ class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home', methods: ['GET'])]
     public function home(
+        Request $request,
         TrickRepository $trickRepository
     ): Response {
-        $tricks = $trickRepository->findBy([], ['createdAt' => 'ASC', 'name' => 'ASC']);
-        return $this->render('home/home.html.twig', compact('tricks'));
+        $page = $request->query->getInt('page', 1);
+        $offset = ($page - 1) * TrickRepository::PAGINATOR_PER_PAGE;
+        $paginator = $trickRepository->getTrickPaginator($offset);
+        return $this->render('home/home.html.twig', [
+            'tricks' => $paginator,
+            'page' => $page,
+            'max' => TrickRepository::PAGINATOR_PER_PAGE,
+        ]);
     }
 }
